d965dfe69430f9a94bce4fbe33d86a9d19073e9c
Daniel Nachun <daniel.nachun@gmail.com>
2026-03-29 11:51:40 -0700

    fix build

--- recipe/build.sh
+++ recipe/build.sh
@@ -5,6 +5,7 @@ set -o xtrace -o nounset -o pipefail -o errexit
 # Create package archive and install globally
 npm pack --ignore-scripts
 npm install -ddd \
+    --no-bin-links \
     --global \
     --build-from-source \
     ${SRC_DIR}/${PKG_NAME}-${PKG_VERSION}.tgz
@@ -13,6 +14,13 @@ npm install -ddd \
 pnpm install
 pnpm-licenses generate-disclaimer --prod --output-file=third-party-licenses.txt
 
+mkdir -p ${PREFIX}/bin
+tee ${PREFIX}/bin/open-cli << EOF
+#!/bin/sh
+exec \${CONDA_PREFIX}/lib/node_modules/open-cli/cli.js "\$@"
+EOF
+chmod +x ${PREFIX}/bin/open-cli
+
 tee ${PREFIX}/bin/open-cli.cmd << EOF
-call %CONDA_PREFIX%\bin\node %CONDA_PREFIX%\bin\open-cli %*
+call %CONDA_PREFIX%\bin\node %CONDA_PREFIX%\lib\node_modules\open-cli\cli.js %*
 EOF
